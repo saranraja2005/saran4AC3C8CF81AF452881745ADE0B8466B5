88d51a1721e82f4b1d557f7f88144cebfd90abff
saranraja2005 <148526378+saranraja2005@users.noreply.github.com>
2023-10-20 12:29:06 +0530

    Add files via upload

---
+++ challenge 1.1.py
@@ -0,0 +1,15 @@
+def factorial(n):
+  if n == 1:
+    return n
+  else:
+    return n * factorial(n - 1)
+
+
+num = int(input("Enter any number: "))
+
+if num < 0:
+  print("Factorial does not exist")
+elif num == 0:
+  print("The factorial of 0 is 1")
+else:
+  print("The factorial of", num, "is", factorial(num))
